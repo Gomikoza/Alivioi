8bf8699ce4431107ff19e539aa8642023f76f074
Anton <yakubovichanton92@gmail>
2023-07-09 13:00:32 +0300

    Alivio
    Alivio

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*mobile nav */
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button-icon');
+const mobileNav = document.querySelector('.mobile-nav')
+
+mobileNavButton.addEventListener('click', function(){
+    mobileNavIcon.classList.toggle('active');
+    mobileNav.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+})
+
+/*video*/
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#story-video-overlay');
+const videoFile = document.querySelector('#video-story');
+
+videoBtn.addEventListener('click', function(){
+
+    function toggleOverlay(event){
+        if(event.type === "mouseleave"){
+            videoOverlay.classList.add('hidden');
+        }else{
+            videoOverlay.classList.remove('hidden');
+        }
+    }
+
+    if(videoFile.paused){
+        videoFile.play();
+        videoBtnIcon.src ="./image/pause-icon.svg"; 
+
+        videoOverlay.onmouseleave = toggleOverlay;
+        videoOverlay.onmouseenter = toggleOverlay;
+
+
+    }else{
+        videoFile.pause();
+        videoBtnIcon.src = "./image/icon-large.svg";
+        videoOverlay.onmouseleave = null;
+        videoOverlay.onmouseenter = null;
+    }
+
+    
+})
